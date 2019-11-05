d6f4f7ededcf6e9f1575b2904e9871bc44fdbced
ashepilova <May01Aug90>
2019-11-05 17:34:03 +0100

    Small MakeUp

--- async_performance_test.py
+++ async_performance_test.py
@@ -1,9 +1,10 @@
 import csv
+import datetime
 import logging
 import os
 import sys
 import time
-import datetime
+
 import matplotlib.pyplot as plt
 import requests
 
@@ -55,6 +56,7 @@ def create_item_request_body(item_id):
             }
         }
     }
+
 
 if __name__ == '__main__':
 
@@ -142,7 +144,8 @@ if __name__ == '__main__':
                     curent_butch_progress = requests.get(host + '/rest/V1/bulk/' + uuid + '/status',
                                                          headers=query_headers)
                     # Check that no of sent requests have status code 4 (Open)
-                    if 4 in [curent_butch_progress.json()["operations_list"][k]["status"] for k in [0, len(curent_butch_progress.json()["operations_list"]) - 1]]:
+                    if 4 in [curent_butch_progress.json()["operations_list"][k]["status"] for k in
+                             [0, len(curent_butch_progress.json()["operations_list"]) - 1]]:
                         logger.info('RunID: ' + str(start_time) + ' Method: ' + method + ' Batch size: ' + str(
                             j) + ' ' + WARNING_PRODUCTS_IN_PROGRESS)
                         some_uuid_open = True
@@ -159,7 +162,7 @@ if __name__ == '__main__':
                 'RunID: ' + str(start_time) + ' Method: ' + method + ' Batch size: ' + str(j) + ' Sending finished')
 
     # Create csv file with elapsed time
-    with open(PATH_TO_SAVE_CSV + 'result_request_time.csv', 'w') as f:
+    with open(PATH_TO_SAVE_CSV + 'summary_response_time.csv', 'w') as f:
         w = csv.writer(f)
         w.writerow(['Batch size'] + methods)
         for i in batch_size_per_run:
@@ -167,16 +170,16 @@ if __name__ == '__main__':
 
     # Create graph with elapsed time
     plt.figure(1, figsize=(12, 7))
-    plt.title('Sum response time for every batch')
+    plt.title('Summary response time for every batch')
     for method_to_show in methods:
         plt.plot(elapsed_sum_per_method_per_run[method_to_show].keys(),
                  [elapsed_sum_per_method_per_run[method_to_show][j]
                   for j in elapsed_sum_per_method_per_run[method_to_show].keys()],
                  label=method_to_show, color=colors[methods.index(method_to_show)])
-    plt.ylabel('Sum response time for batch, s')
+    plt.ylabel('Summary response time for batch, s')
     plt.xlabel('Sent batch size')
     plt.legend(loc='upper left')
-    plt.savefig(PATH_TO_SAVE_IMAGES + 'Sum response time for every batch.png')
+    plt.savefig(PATH_TO_SAVE_IMAGES + 'Summary response time.png')
 
     # Total time calculation
     for method in start_timestamps.keys():
@@ -217,7 +220,7 @@ if __name__ == '__main__':
         plt.plot(total_time[method_to_show].keys(),
                  [total_time[method_to_show][j] for j in total_time[method_to_show].keys()],
                  label=method_to_show, color=colors[methods.index(method_to_show)])
-    plt.ylabel('Total time for batch, s')
+    plt.ylabel('Total time for batch creation, s')
     plt.xlabel('Sent batch size')
     plt.legend(loc='upper left')
     plt.savefig(PATH_TO_SAVE_IMAGES + 'Total time.png')
@@ -231,7 +234,7 @@ if __name__ == '__main__':
 
     # Create graph with success percentage
     plt.figure(3, figsize=(12, 7))
-    plt.title('Total success created percentage')
+    plt.title('Total success created items percentage')
     for method_to_show in methods:
         plt.plot(success_created_percentage[method_to_show].keys(),
                  [success_created_percentage[method_to_show][j] for j in
@@ -240,4 +243,4 @@ if __name__ == '__main__':
     plt.ylabel('Total success created items, %')
     plt.xlabel('Sent batch size')
     plt.legend(loc='upper left')
-    plt.savefig(PATH_TO_SAVE_IMAGES + 'Total success created percentage.png')
+    plt.savefig(PATH_TO_SAVE_IMAGES + 'Total success created items percentage.png')
