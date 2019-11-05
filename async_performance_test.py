11f195d9e91fe5609cae9432db57591d7d5da09f
ashepilova <May01Aug90>
2019-11-05 17:07:57 +0100

    Add comments

--- async_performance_test.py
+++ async_performance_test.py
@@ -117,6 +117,7 @@ if __name__ == '__main__':
                 start_time = timestamp_s()
                 logger.info(
                     'RunID: ' + str(start_time) + ' Method: ' + method + ' Batch size: ' + str(j) + ' Sending started')
+                # Sync requsts sending one by one
                 for i in range(1, j + 1):
                     item_id = method + "_" + str(start_time) + "_" + str(i)
                     request = create_item_request_body(item_id)
@@ -128,6 +129,7 @@ if __name__ == '__main__':
                 start_time = timestamp_s()
                 logger.info(
                     'RunID: ' + str(start_time) + ' Method: ' + method + ' Batch size: ' + str(j) + ' Sending started')
+                # Async requsts sending one by one
                 for i in range(1, j + 1):
                     item_id = method + "_" + str(start_time) + "_" + str(i)
                     request = create_item_request_body(item_id)
@@ -141,9 +143,11 @@ if __name__ == '__main__':
                 start_time = timestamp_s()
                 logger.info(
                     'RunID: ' + str(start_time) + ' Method: ' + method + ' Batch size: ' + str(j) + ' Sending started')
+                # Request body for bulk creating
                 for i in range(1, j + 1):
                     item_id = method + "_" + str(start_time) + "_" + str(i)
                     request.append(create_item_request_body(item_id))
+                # Bulk request sending
                 response = requests.post(host + '/rest/async/bulk/V1/products', headers=query_headers, json=request)
                 bulk_uuids_current_run.append(response.json()['bulk_uuid'])
                 elapsed_current_run.append(response.elapsed.total_seconds())
@@ -152,6 +156,7 @@ if __name__ == '__main__':
                 logger.info('Method: ' + method + ' is unexpected method')
                 continue
 
+            # Controll that async/bulk requests have been processed before new batch start
             some_uuid_open = True
             time_to_timeout = time.time() + TIMEOUT_S
             while some_uuid_open:
@@ -164,6 +169,7 @@ if __name__ == '__main__':
                 for uuid in bulk_uuids_current_run:
                     curent_butch_progress = requests.get(host + '/rest/V1/bulk/' + uuid + '/status',
                                                          headers=query_headers)
+                    # Check that no of sent requests have status code 4 (Open)
                     if 4 in [curent_butch_progress.json()["operations_list"][k]["status"] for k in [0, len(curent_butch_progress.json()["operations_list"]) - 1]]:
                         logger.info('RunID: ' + str(start_time) + ' Method: ' + method + ' Batch size: ' + str(
                             j) + ' ' + WARNING_PRODUCTS_IN_PROGRESS)
@@ -171,19 +177,23 @@ if __name__ == '__main__':
                         time.sleep(3)
                         break
                     else:
+                        # In case some request already done (not 4) it shouldn't be checked next time
                         bulk_uuids_current_run.remove(uuid)
-
+            # Elapsed time for whole batch calculation
             elapsed_sum_per_method_per_run[method][j] = sum(elapsed_current_run)
+            # Save start timestamp as id of run
             start_timestamps[method][j] = start_time
             logger.info(
                 'RunID: ' + str(start_time) + ' Method: ' + method + ' Batch size: ' + str(j) + ' Sending finished')
 
+    # Create csv file with elapsed time
     with open(PATH_TO_SAVE_CSV + 'result_request_time.csv', 'w') as f:
         w = csv.writer(f)
         w.writerow(['Batch size'] + methods)
         for i in batch_size_per_run:
             w.writerow([i] + [elapsed_sum_per_method_per_run[j].get(i, 'NaN') for j in methods])
 
+    # Create graph with elapsed time
     plt.figure(1, figsize=(12, 7))
     plt.title('Sum response time for every batch')
     for method_to_show in methods:
@@ -196,20 +206,23 @@ if __name__ == '__main__':
     plt.legend(loc='upper left')
     plt.savefig(PATH_TO_SAVE_IMAGES + 'Sum response time for every batch.png')
 
+    # Total time calculation
     for method in start_timestamps.keys():
-        for batch_size in start_timestamps[method].keys():
-            start_timestamp = start_timestamps[method][batch_size]
+        for batch_size in start_timestamps[method].keys():  # For every batch
+            start_timestamp = start_timestamps[method][batch_size]  # Extract start time
             endpoint = host + '/rest/V1/products?searchCriteria[pageSize]=' + str(batch_size) + \
                        '&searchCriteria[filterGroups][0][filters][0][field]=sku&searchCriteria[filterGroups][0][filters][0][value]=' + \
                        method + '_' + str(
                 start_timestamp) + '%25&searchCriteria[filterGroups][0][filters][0][condition_type]=like'
+            # Request created products by run id
             request = requests.get(endpoint, headers=query_headers)
-            created_orders_per_run = request.json()
-            created_items_count = int(created_orders_per_run['total_count'])
+            created_items_per_batch = request.json()
+            created_items_count = int(created_items_per_batch['total_count'])
             success_created_percentage[method][batch_size] = created_items_count * 100 / batch_size
+            # If some items were created save max time and calculate total time, else save 0 as total time
             if created_items_count > 0:
                 max_timestamp_per_run = max([datetime.timestamp(
-                    datetime.strptime(created_orders_per_run['items'][k]['created_at'], '%Y-%m-%d %H:%M:%S'))
+                    datetime.strptime(created_items_per_batch['items'][k]['created_at'], '%Y-%m-%d %H:%M:%S'))
                     for k in range(0, created_items_count)])
                 total_time[method][batch_size] = int(max_timestamp_per_run - start_timestamp)
             else:
@@ -218,12 +231,14 @@ if __name__ == '__main__':
                         batch_size) + ' There\'s no item was created. ')
                 total_time[method][batch_size] = 0
 
+    # Create csv file with total time
     with open(PATH_TO_SAVE_CSV + 'total_time.csv', 'w') as f:
         w = csv.writer(f)
         w.writerow(['Batch size'] + methods)
         for i in batch_size_per_run:
             w.writerow([i] + [total_time[j].get(i, 'NaN') for j in methods])
 
+    # Create graph with total time
     plt.figure(2, figsize=(12, 7))
     plt.title('Total time')
     for method_to_show in methods:
@@ -235,12 +250,14 @@ if __name__ == '__main__':
     plt.legend(loc='upper left')
     plt.savefig(PATH_TO_SAVE_IMAGES + 'Total time.png')
 
+    # Create csv file with success percentage
     with open(PATH_TO_SAVE_CSV + 'total_success_percentage.csv', 'w') as f:
         w = csv.writer(f)
         w.writerow(['Batch size'] + methods)
         for i in batch_size_per_run:
             w.writerow([i] + [success_created_percentage[j].get(i, 'NaN') for j in methods])
 
+    # Create graph with success percentage
     plt.figure(3, figsize=(12, 7))
     plt.title('Total success created percentage')
     for method_to_show in methods:
